54a19586c0175781dc93774c88c0c4cbaee54033
Mauricio Vásquez <mauriciov@microsoft.com>
2023-11-02 13:51:43 -0500

    fix

--- 2023-rejekts-na/myopensnoop/base/program.bpf.c
+++ 2023-rejekts-na/myopensnoop/base/program.bpf.c
@@ -5,14 +5,11 @@
 
 // Inspektor Gadget macros
 #include <gadget/macros.h>
-
-#include <gadget/mntns_filter.h>
 #include <gadget/types.h>
 
 #define NAME_MAX 255
 
 struct event {
-	//gadget_mntns_id mntns;
 	__u32 pid;
 	__u8 name[NAME_MAX];
 	__u8 comm[TASK_COMM_LEN];
@@ -31,13 +28,7 @@ int enter_openat(struct trace_event_raw_sys_enter *ctx)
 {
 	struct event evt = {};
 
-	evt.mntns = gadget_get_mntns_id();
-	if (gadget_should_discard_mntns_id(evt.mntns)) {
-		return 0;
-	}
-
 	evt.pid = bpf_get_current_pid_tgid() >> 32;
-
 	bpf_get_current_comm(&evt.comm, sizeof(evt.comm));
 	bpf_probe_read_user_str(&evt.name, sizeof(evt.name), (const char *)ctx->args[1]);
 
